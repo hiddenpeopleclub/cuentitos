2ff5df9fc6906a5d5118f1d8d66b2b01541d5d62
loomstyla <pabloferrari.hh@gmail.com>
2023-04-21 19:50:03 -0300

    Identation update

--- palabritas-parser/src/main.rs
+++ palabritas-parser/src/main.rs
@@ -9,9 +9,8 @@ use pest::Parser;
 pub struct PalabritasParser;
 
 fn main() {
-  let unparsed_file =
-    std::fs::read_to_string("../examples/story-example.cuentitos").expect("cannot read file");
-  PalabritasParser::parse(Rule::file, &unparsed_file)
+  let unparsed_file = include_str!("../../examples/story-example.cuentitos");
+  PalabritasParser::parse(Rule::File, unparsed_file)
     .expect("unsuccessful parse") // unwrap the parse result
     .next()
     .unwrap();
@@ -38,10 +37,10 @@ mod test {
   fn parse_char() {
     //char = { ASCII_ALPHANUMERIC | "." | "_" | "/" | "," | ";" | "'" | " " | "?" | "!" | "¿" | "¡"}
     let alphanumeric_char = (rand::thread_rng().sample(&Alphanumeric) as char).to_string();
-    assert_parse(Rule::char, &alphanumeric_char);
+    assert_parse(Rule::Char, &alphanumeric_char);
 
     for special_character in SPECIAL_CHARACTERS {
-      assert_parse(Rule::char, special_character);
+      assert_parse(Rule::Char, special_character);
     }
   }
 
@@ -49,7 +48,7 @@ mod test {
   fn parse_integer() {
     //integer = { "-"? ~ ASCII_DIGIT+ }
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
-    assert_parse(Rule::integer, &integer);
+    assert_parse(Rule::Integer, &integer);
   }
 
   #[test]
@@ -58,66 +57,66 @@ mod test {
     let float = rand::thread_rng()
       .gen_range(i8::MIN as f32..i8::MAX as f32)
       .to_string();
-    assert_parse(Rule::float, &float);
+    assert_parse(Rule::Float, &float);
   }
 
   #[test]
   fn parse_percentage() {
     //percentage = { integer ~ "%" }
     let percentage = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string() + "%";
-    assert_parse(Rule::percentage, &percentage);
+    assert_parse(Rule::Percentage, &percentage);
   }
 
   #[test]
   fn parse_indentation() {
     //indentation = { "  " | "\t" }
     for indentation in INDENTATIONS {
-      assert_parse(Rule::indentation, &indentation);
+      assert_parse(Rule::Indentation, &indentation);
     }
   }
 
   #[test]
   fn parse_string() {
     //string = { char+ }
-    assert_parse(Rule::string, &make_random_string());
+    assert_parse(Rule::String, &make_random_string());
   }
 
   #[test]
   fn parse_comparison_operator() {
     //comparison_operator = { "!=" | "=" | "<=" | ">=" | "<" | ">" | "!" }
     for operator in COMPARISON_OPERATORS {
-      assert_parse(Rule::comparison_operator, operator);
+      assert_parse(Rule::ComparisonOperator, operator);
     }
   }
 
   #[test]
   fn parse_snake_case() {
     //snake_case = { ASCII_ALPHA_LOWER ~ (ASCII_ALPHA_LOWER | "_" | ASCII_DIGIT)* }
-    assert_parse(Rule::snake_case, &make_random_snake_case());
+    assert_parse(Rule::SnakeCase, &make_random_snake_case());
   }
 
   #[test]
   fn parse_identifier() {
     //identifier = { (ASCII_ALPHA | "_" ) ~ (ASCII_ALPHANUMERIC | "_")* }
-    assert_parse(Rule::identifier, &make_random_identifier());
+    assert_parse(Rule::Identifier, &make_random_identifier());
   }
 
   #[test]
   fn parse_value() {
     //value = { identifier | float | percentage | integer}
     let identifier = make_random_identifier();
-    assert_parse(Rule::value, &identifier);
+    assert_parse(Rule::Value, &identifier);
 
     let float = rand::thread_rng()
       .gen_range(i8::MIN as f32..i8::MAX as f32)
       .to_string();
-    assert_parse(Rule::value, &float);
+    assert_parse(Rule::Value, &float);
 
     let percentage = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string() + "%";
-    assert_parse(Rule::value, &percentage);
+    assert_parse(Rule::Value, &percentage);
 
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
-    assert_parse(Rule::value, &integer);
+    assert_parse(Rule::Value, &integer);
   }
 
   #[test]
@@ -128,9 +127,9 @@ mod test {
       COMPARISON_OPERATORS[rand::thread_rng().gen_range(0..COMPARISON_OPERATORS.len())];
     let value = make_random_identifier();
 
-    assert_parse(Rule::condition, &(identifier.clone() + " " + &value));
+    assert_parse(Rule::Condition, &(identifier.clone() + " " + &value));
     assert_parse(
-      Rule::condition,
+      Rule::Condition,
       &(identifier + comparison_operator + &value),
     );
   }
@@ -139,7 +138,7 @@ mod test {
   fn parse_requirement() {
     //requirement = { "req" ~ " "+ ~ condition }
     let condition = make_random_condition();
-    assert_parse(Rule::requirement, &("req ".to_string() + &condition));
+    assert_parse(Rule::Requirement, &("req ".to_string() + &condition));
   }
   #[test]
   fn parse_frequency() {
@@ -151,11 +150,11 @@ mod test {
       .to_string();
 
     assert_parse(
-      Rule::frequency,
+      Rule::Frequency,
       &("freq ".to_string() + &condition + " " + &integer),
     );
     assert_parse(
-      Rule::frequency,
+      Rule::Frequency,
       &("freq ".to_string() + &condition + " " + &float),
     );
   }
@@ -167,7 +166,7 @@ mod test {
     let value = make_random_identifier();
 
     assert_parse(
-      Rule::modifier,
+      Rule::Modifier,
       &("mod ".to_string() + &identifier + " " + &value),
     );
   }
@@ -178,66 +177,54 @@ mod test {
     let knot = make_random_identifier();
     let stitch = make_random_identifier();
 
-    assert_parse(Rule::divert, &("->".to_string() + &knot));
-    assert_parse(Rule::divert, &("->".to_string() + &knot + "." + &stitch));
+    assert_parse(Rule::Divert, &("->".to_string() + &knot));
+    assert_parse(Rule::Divert, &("->".to_string() + &knot + "." + &stitch));
   }
 
   #[test]
   fn parse_command() {
-    //command = { NEWLINE ~ indentation* ~ (requirement | frequency | modifier | divert) ~ " "*}
+    //command = { (requirement | frequency | modifier | divert) }
     let requirement = "req ".to_string() + &(make_random_condition());
-    assert_parse(
-      Rule::command,
-      &("\n".to_string() + &make_random_indentation() + &requirement),
-    );
+    assert_parse(Rule::Command, &(requirement));
 
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
     let frequency = "freq ".to_string() + &make_random_condition() + " " + &integer;
-    assert_parse(
-      Rule::command,
-      &("\n".to_string() + &make_random_indentation() + &frequency),
-    );
+    assert_parse(Rule::Command, &(frequency));
 
     let modifier = "mod ".to_string() + &make_random_identifier() + " " + &make_random_identifier();
-    assert_parse(
-      Rule::command,
-      &("\n".to_string() + &make_random_indentation() + &modifier),
-    );
+    assert_parse(Rule::Command, &(modifier));
 
     let divert = "->".to_string() + &make_random_identifier();
-    assert_parse(
-      Rule::command,
-      &("\n".to_string() + &make_random_indentation() + &divert),
-    );
+    assert_parse(Rule::Command, &(divert));
   }
 
   #[test]
   fn parse_probability() {
     //probability = { "(" ~ " "* ~ ( percentage | float | integer ) ~ " "* ~ ")" ~ " "* }
     let percentage = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string() + "%";
-    assert_parse(Rule::probability, &("(".to_string() + &percentage + ")"));
+    assert_parse(Rule::Probability, &("(".to_string() + &percentage + ")"));
 
     let float = rand::thread_rng()
       .gen_range(i8::MIN as f32..i8::MAX as f32)
       .to_string();
-    assert_parse(Rule::probability, &("(".to_string() + &float + ")"));
+    assert_parse(Rule::Probability, &("(".to_string() + &float + ")"));
 
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
-    assert_parse(Rule::probability, &("(".to_string() + &integer + ")"));
+    assert_parse(Rule::Probability, &("(".to_string() + &integer + ")"));
   }
 
   #[test]
   fn parse_knot() {
     //knot = {"===" ~ " "* ~ identifier ~ " "* ~"===" ~ " "*}
     let identifier = make_random_identifier();
-    assert_parse(Rule::knot, &("===".to_string() + &identifier + "==="));
+    assert_parse(Rule::Knot, &("===".to_string() + &identifier + "==="));
   }
 
   #[test]
   fn parse_stitch() {
     //stitch = {"=" ~ " "* ~ identifier ~ " "*}
     let identifier = make_random_identifier();
-    assert_parse(Rule::stitch, &("=".to_string() + &identifier));
+    assert_parse(Rule::Stitch, &("=".to_string() + &identifier));
   }
 
   #[test]
@@ -245,72 +232,66 @@ mod test {
     //text = { probability? ~ string }
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
     let probability = "(".to_string() + &integer + ")";
-    assert_parse(Rule::text, &make_random_string());
-    assert_parse(Rule::text, &(probability + &make_random_string()));
+    assert_parse(Rule::Text, &make_random_string());
+    assert_parse(Rule::Text, &(probability + &make_random_string()));
   }
 
   #[test]
   fn parse_option() {
-    //option = { indentation* ~ "*" ~ " "* ~ text ~ command* }
+    //option = { "*" ~ " "* ~ text }
     let text = make_random_string();
-    let command = "\nreq ".to_string() + &(make_random_condition());
-    assert_parse(Rule::option, &(make_random_indentation() + "*" + &text));
-    assert_parse(
-      Rule::option,
-      &(make_random_indentation() + "*" + &text + &command),
-    );
-  }
-
-  #[test]
-  fn parse_text_shown() {
-    //text_shown = { indentation* ~ text ~ command* }
-    let text = make_random_string();
-    let command = "\nreq ".to_string() + &(make_random_condition());
-    assert_parse(Rule::text_shown, &(make_random_indentation() + &text));
-    assert_parse(
-      Rule::text_shown,
-      &(make_random_indentation() + &text + &command),
-    );
+    assert_parse(Rule::Option, &("*".to_string() + &text));
   }
 
   #[test]
   fn parse_named_bucket() {
-    //named_bucket = { indentation* ~ "[" ~ " "* ~ probability? ~ snake_case ~ " "* ~ "]" ~ " "* ~ command* }
-    let command = "\nreq ".to_string() + &(make_random_condition());
+    //named_bucket = { "[" ~ " "* ~ probability? ~ snake_case ~ " "* ~ "]"}
     let integer = rand::thread_rng().gen_range(i8::MIN..i8::MAX).to_string();
     let probability = "(".to_string() + &integer + ")";
 
     assert_parse(
-      Rule::named_bucket,
-      &(make_random_indentation() + "[" + &make_random_snake_case() + "]"),
-    );
-    assert_parse(
-      Rule::named_bucket,
-      &(make_random_indentation() + "[" + &make_random_snake_case() + "]" + &command),
+      Rule::NamedBucket,
+      &("[".to_string() + &make_random_snake_case() + "]"),
     );
 
     assert_parse(
-      Rule::named_bucket,
-      &(make_random_indentation() + "[" + &probability + &make_random_snake_case() + "]"),
-    );
-
-    assert_parse(
-      Rule::named_bucket,
-      &(make_random_indentation()
-        + "["
-        + &probability
-        + &make_random_snake_case()
-        + "]"
-        + &command),
+      Rule::NamedBucket,
+      &("[".to_string() + &probability + &make_random_snake_case() + "]"),
     );
   }
 
   #[test]
+  fn parse_block_content() {
+    //BlockContent = {
+    //  (Command | NamedBucket | Option | Knot | Stitch | Text)  ~  " "* ~ (NEWLINE | EOI) ~ NewBlock*
+    //}
+    let command = "req ".to_string() + &(make_random_condition());
+    assert_parse(Rule::BlockContent, &command);
+
+    let named_bucket = "[".to_string() + &make_random_snake_case() + "]";
+    assert_parse(Rule::BlockContent, &named_bucket);
+
+    let option = "*".to_string() + &make_random_string();
+    assert_parse(Rule::BlockContent, &option);
+
+    let knot = "===".to_string() + &make_random_identifier() + "===";
+    assert_parse(Rule::BlockContent, &knot);
+
+    let stitch = "=".to_string() + &make_random_identifier();
+    assert_parse(Rule::BlockContent, &stitch);
+
+    let text = make_random_string();
+    assert_parse(Rule::BlockContent, &text);
+
+    let new_block = "\n  ".to_string() + &make_random_string();
+    assert_parse(Rule::BlockContent, &(text + &new_block));
+  }
+
+  #[test]
   fn parse_file() {
-    //file = { SOI ~ (( named_bucket | text_shown | option | indentation+ | knot | stitch)? ~ NEWLINE)* ~ (named_bucket | text_shown | option | indentation+ | knot | stitch)? ~ EOI }
-    let unparsed_file =
-      std::fs::read_to_string("../examples/story-example.cuentitos").expect("cannot read file");
-    assert_parse(Rule::file, &unparsed_file);
+    //file = { SOI ~ (NEWLINE | BlockContent)* ~ EOI }
+    let unparsed_file = include_str!("../../examples/story-example.cuentitos");
+    assert_parse(Rule::File, &unparsed_file);
   }
 
   fn assert_parse(rule: Rule, input: &str) {
@@ -322,16 +303,6 @@ mod test {
     assert_eq!(pair.as_str(), input);
   }
 
-  fn make_random_indentation() -> String {
-    let indentation_count = rand::thread_rng().gen_range(0..20);
-    let mut indentation = String::default();
-
-    for _ in 0..indentation_count {
-      indentation += "  ";
-    }
-
-    indentation
-  }
   fn make_random_condition() -> String {
     make_random_identifier() + " " + &make_random_identifier()
   }
@@ -347,7 +318,7 @@ mod test {
       .collect();
 
     let mut snake_case = std::str::from_utf8(&snake_case).unwrap().to_string();
-
+    
     //Adding underscores
     for _ in 0..underscore_size {
       snake_case += "_";
