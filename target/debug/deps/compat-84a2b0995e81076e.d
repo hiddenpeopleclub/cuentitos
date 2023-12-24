d248a997454495d38373ca5db908c263a8066629
Fran Tufro <hi@frantufro.com>
2023-12-24 16:07:26 -0300

    Added website, removed target.

--- target/debug/deps/compat-84a2b0995e81076e.d
+++
@@ -1,5 +0,0 @@
-/Users/frantufro/hidden-people-club/cuentitos/target/debug/deps/compat-84a2b0995e81076e: compat/src/main.rs
-
-/Users/frantufro/hidden-people-club/cuentitos/target/debug/deps/compat-84a2b0995e81076e.d: compat/src/main.rs
-
-compat/src/main.rs:
