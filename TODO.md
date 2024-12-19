3403fbd83be47e61bfa58d5c7d7cb020d8dae7d7
Fran Tufro <hi@frantufro.com>
2024-12-19 17:54:33 -0300

    Added Runtime base

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Compat
+  - [ ] Add a `--verbose` option that shows more information about the test
+        execution.
--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,6 +1,34 @@
-use std::env;
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+/// Simple program to greet a person
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+  #[command(subcommand)]
+  command: Commands
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+  /// does testing things
+  Run {
+    /// lists test values
+    script_path: PathBuf,
+    input_string: Option<String>
+  },
+}
+
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-     dbg!(args);
+  let cli = Args::parse();
+
+  match cli.command {
+    Commands::Run { script_path, input_string } => {
+      println!("Running script: {:?}", script_path);
+      if let Some(input) = input_string {
+        println!("Input string: {}", input);
+      }
+    }
+  }
 }
